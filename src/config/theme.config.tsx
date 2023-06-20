d5bf5672805ebd25775960486e35b27bf2305671
Abraham Huacchillo <huacchillocastillo@gmail.com>
2023-06-19 19:06:31 -0500

    add some styles

--- src/config/theme.config.tsx
+++ src/config/theme.config.tsx
@@ -1,17 +1,39 @@
-import { ThemeProvider, createTheme } from "@mui/material";
+
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import React from "react";
 
 type ThemeProps = {
     children: JSX.Element
 }
 
-const theme = createTheme({
+export enum ThemePalette {
+        BG = "#12181b",
+        LIME ="#c8fa5f",
+        FONT_GLOBAL= " 'JetBrains Mono', monospace"
+}
 
+const theme = createTheme({
+    palette: {
+        mode: 'dark',
+        background: { 
+            default: ThemePalette.BG,
+        },
+        primary: {  
+            main: ThemePalette.LIME, 
+        },
+    },
+    typography: {
+        fontFamily: ThemePalette.FONT_GLOBAL,
+    },
+    components: {
+        MuiButton: {},
+    },            
 });
 
 export const ThemeConfig: React.FC<ThemeProps> = ({children}) => {
     return (
         <ThemeProvider theme={theme}>
+            <CssBaseline  />
             {children}
         </ThemeProvider>
     )
