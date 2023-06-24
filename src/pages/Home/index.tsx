eec3cc04ac0ce5d6df8e48b1d9469a1c0f789a92
Abraham Huacchillo <huacchillocastillo@gmail.com>
2023-06-23 21:48:12 -0500

    In this commit, I added pagination using Material UI.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,22 +1,31 @@
 import React from 'react';
-import { Box, Button, Card, Container, Grid } from '@mui/material';
+import { Box, Button, Card, CircularProgress, Container, Grid, Pagination, Stack } from '@mui/material';
 import { CardComponent, HeaderContainer } from '../../components';
 import { getCharacters } from '../../api/characters';
 import { TypeCharacter } from './interface/character.interface';
 
 export const HomePage: React.FC = () => {
   const [allCharacters, setAllCharacters] = React.useState<TypeCharacter[] | null > (null)
+  const [loading, setLoading] = React.useState<boolean>(true)
+  const [page, setPage] = React.useState<number>(1)
+  const [count, setCount] = React.useState<number>(1)
+
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+  }
 
   React.useEffect(() => {
-    getCharacters.getAll({ page: 1 })
+    setLoading(true);
+    getCharacters.getAll({ page })
       .then((response) => {
-        console.log(response.data.results);
+        setCount(response.data.info.pages);
         setAllCharacters(response.data.results);
+        setTimeout(() => setLoading(false), 1000);
       })
       .catch((error) => {
         console.log(error);
       });
-  }, [])
+  }, [page])
 
     return(
         <Container sx={{mt:9}} maxWidth="xl">
@@ -25,19 +34,32 @@ export const HomePage: React.FC = () => {
         description='hi everyone, this my first app with typescript'
         element={<Button variant='contained' color='primary'>Learn More</Button>}
         />
-        <Grid container spacing={2} direction="row" >
-          {allCharacters?.map((character) => (
-            <Grid item xs={3}>
-              <CardComponent
+        {
+          loading ? (
+            <Box sx={{ display: 'flex', justifyContent: 'center', mt:4 }}>
+              <CircularProgress />
+            </Box>
+          ) :(
+            <>
+            <Grid container spacing={2} direction="row" mt={10} >
+              {allCharacters?.map((character) => (
+              <Grid item xs={3}>
+                <CardComponent
                 key={character.id}
                 image={character.image}
                 name={character.name}
                 species={character.species}
                 status={character.status}
-              />
+                />
+              </Grid>
+              ))}
           </Grid>
-        ))}
-        </Grid>
+          <Box sx={{width:'100%', display:'flex', justifyContent:'center', mb:10}}>
+            <Pagination count={count} color="primary" sx={{mt:4}} page={page} onChange={handleChange} />
+          </Box>
+        </>
+          )
+        }
       </Container>
     )
 }
