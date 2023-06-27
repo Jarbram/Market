9c40a394ca5ec8e24f197323f31c04dcd99e286b
Abraham Huacchillo <huacchillocastillo@gmail.com>
2023-06-26 19:57:15 -0500

    In this commit, I implemented Formik to improve the efficiency of form validation.

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -2,37 +2,25 @@ import React from 'react';
 import { Box, Button, Container, Grid, Paper, TextField, Typography } from '@mui/material';
 import { UseNotification } from '../../context/notification.context';
 import { LoginValidate } from '../../utils/validateForm';
+import { useFormik } from 'formik';
+
 type LoginType = {
   email: string,
   password: string
 }
 
 export const LoginPage: React.FC = () => {
-  const {getError,getSuccess} = UseNotification()
-
-
-
-  const [loginData, setLoginData] = React.useState<LoginType>({
+const { getSuccess } = UseNotification();
+const formik = useFormik<LoginType>({
+  initialValues: {
     email: '',
-    password: '',
-  })
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setLoginData({
-      ...loginData,
-      [event.target.name]: event.target.value
-    })
-  }
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    LoginValidate.validate(loginData).then((data) => {
-    getSuccess(JSON.stringify(loginData))
-    }).catch((err) => {
-      getError(err.message)
-    }
-    )
-  }
+    password: ''
+  },
+  validationSchema: LoginValidate,
+  onSubmit: (values : LoginType) => {
+  getSuccess(JSON.stringify(values))
+  },
+})
 
     return(
         <Container  maxWidth="sm">
@@ -51,13 +39,15 @@ export const LoginPage: React.FC = () => {
                 >
                   Iniciar Sesión
                 </Typography>
-                <Box component="form" onSubmit={handleSubmit} >
+                <Box component="form" onSubmit={formik.handleSubmit} >
                   <TextField 
                   fullWidth 
                   label="Email" 
                   name='email'
-                  value={loginData.email}
-                  onChange={handleChange}
+                  onChange={formik.handleChange}
+                  value={formik.values.email}
+                  error={formik.touched.email && Boolean(formik.errors.email)}
+                  helperText={formik.touched.email && formik.errors.email}
                   sx={{mt:2,mb:2}} 
                   type='email'
                   margin='normal'
@@ -66,8 +56,10 @@ export const LoginPage: React.FC = () => {
                   fullWidth 
                   label="Password" 
                   name='password'
-                  value={loginData.password}
-                  onChange={handleChange}
+                  onChange={formik.handleChange}
+                  value={formik.values.password}
+                  error={formik.touched.password && Boolean(formik.errors.password)}
+                  helperText={formik.touched.password && formik.errors.password}
                   sx={{mt:1.5,mb:1.5}} 
                   type="password"
                   margin='normal'
